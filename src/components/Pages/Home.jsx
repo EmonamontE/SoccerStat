166904af656cdf4514bc5ac4b1936d72c640ddf6
emonamonte <racoonsmr@gmail.com>
2021-09-06 00:13:56 +0400

    Home if not a class component anymore

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,58 +1,56 @@
 import React from "react";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <div>
-          <h1
-            className="py-1 text-center text-light bg-primary"
-          >
-            Добро пожаловать на SoccerStat!
-          </h1>
-          <p className="fs-4">
-            В данном приложении применяется API с бесплатным доступом.
-            Количество запросов: 10 шт/мин.
-            При достижении лимита приложение вернет ошибку.
-          </p>
-        </div>
-
-        <br/>
-
-        <div>
-          <h2>Список лиг</h2>
-          <p className="fs-4">
-            Беслатный тарифный план дает доступ о данных только 12 лиг.
-            Их список приведен чуть ниже. При выборе лиги Вам будет представлен календарь
-            матчей текущего сезона. Попытка получить календарь любой другой лиги
-            вернет ошибку.
-          </p>
-          <ul className="col-md-4 list-group fs-5">
-            <li className="list-group-item">UEFA Champions League</li>
-            <li className="list-group-item">Primeira Liga</li>
-            <li className="list-group-item">Premier League</li>
-            <li className="list-group-item">Eredivisie</li>
-            <li className="list-group-item">Bundesliga</li>
-            <li className="list-group-item">Ligue 1</li>
-            <li className="list-group-item">Serie A</li>
-            <li className="list-group-item">Primera Division</li>
-            <li className="list-group-item">Championship</li>
-            <li className="list-group-item">Campeonato Brasileiro Série A</li>
-            <li className="list-group-item">FIFA World Cup</li>
-            <li className="list-group-item">European Championship</li>
-          </ul>
-        </div>
-
-        <br/>
-
-        <h2>Список команд</h2>
+function Home() {
+  return (
+    <div className="container">
+      <div>
+        <h1
+          className="py-1 text-center text-light bg-primary"
+        >
+          Добро пожаловать на SoccerStat!
+        </h1>
         <p className="fs-4">
-          Вам будет предложен список из команд английской премьер лиги.
-          По нажатию на команду вы увидите календарь ее игр.
+          В данном приложении применяется API с бесплатным доступом.
+          Количество запросов: 10 шт/мин.
+          При достижении лимита приложение вернет ошибку.
         </p>
       </div>
-    );
-  }
+
+      <br/>
+
+      <div>
+        <h2>Список лиг</h2>
+        <p className="fs-4">
+          Беслатный тарифный план дает доступ о данных только 12 лиг.
+          Их список приведен чуть ниже. При выборе лиги Вам будет представлен календарь
+          матчей текущего сезона. Попытка получить календарь любой другой лиги
+          вернет ошибку.
+        </p>
+        <ul className="col-md-4 list-group fs-5">
+          <li className="list-group-item">UEFA Champions League</li>
+          <li className="list-group-item">Primeira Liga</li>
+          <li className="list-group-item">Premier League</li>
+          <li className="list-group-item">Eredivisie</li>
+          <li className="list-group-item">Bundesliga</li>
+          <li className="list-group-item">Ligue 1</li>
+          <li className="list-group-item">Serie A</li>
+          <li className="list-group-item">Primera Division</li>
+          <li className="list-group-item">Championship</li>
+          <li className="list-group-item">Campeonato Brasileiro Série A</li>
+          <li className="list-group-item">FIFA World Cup</li>
+          <li className="list-group-item">European Championship</li>
+        </ul>
+      </div>
+
+      <br/>
+
+      <h2>Список команд</h2>
+      <p className="fs-4">
+        Вам будет предложен список из команд английской премьер лиги.
+        По нажатию на команду вы увидите календарь ее игр.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
